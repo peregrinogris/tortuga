b39d2bb7d2143233d2bb3460cb53158f64ea307a
Hernan Rodriguez Colmeiro <colmeiro@gmail.com>
2015-11-22 00:52:04 -0300

    Add spiral example.

---
+++ js/spiral.js
@@ -0,0 +1,21 @@
+var turtle = new Turtle('#canvas', 400, 300);
+
+function poly(angle, length) {
+  for (let i=0; i<360/angle; i++){
+    turtle.forward(length);
+    turtle.right(angle);
+    length += 2;
+  }
+  return length;
+}
+
+function spiral(angle, initialLength){
+  for (let i=0; i<=30; i++){
+    turtle.color('rgb(0, '+ (15 + 8 * i) +', '+ (255 - 8 * i) +')');
+    initialLength = poly(angle, initialLength);
+    turtle.drawPath();
+  }
+}
+
+
+spiral(92, 30);
