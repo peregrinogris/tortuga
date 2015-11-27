b421f158994a2bf5e2c14d8b9a3a7446f3e6b6d8
Hernan Rodriguez Colmeiro <colmeiro@gmail.com>
2015-11-27 11:24:41 -0300

    Draw: Added a penUp/penDown example.

--- js/draw.js
+++ js/draw.js
@@ -7,9 +7,32 @@ function poly(angle, size) {
   }
 }
 
+function dashedPoly(angle){
+  let totalTurning = 0;
+  do {
+    for (let i=0; i<=30; i++){
+      turtle.color('rgb(0, '+ (15 + 8 * i) +', '+ (255 - 8 * i) +')');
+      turtle.forward(10);
+      turtle.penUp();
+      turtle.right(90);
+      turtle.forward(10);
+      turtle.left(90);
+      turtle.penDown();
+    }
+    turtle.right(angle);
+    totalTurning = (totalTurning + angle) % 360;
+  } while (totalTurning > 0);
+}
+
+// Create a figure using poly
 for (let i=0; i<=30; i++){
   turtle.color('rgb(0, '+ (15 + 8 * i) +', '+ (255 - 8 * i) +')');
   poly(27, 50);
   turtle.right(100);
-  turtle.drawPath();
 }
+turtle.drawPath();
+
+// Dashed 5 pointed star
+turtle.direction = 90;
+turtle.position = [400, 550];
+dashedPoly(144);
